2e17767611207361a5c93373be6ff763f5e4205d
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2017-09-27 21:47:29 +0300

    unify B, support LDR, set flags correctly for MOV

--- src/executor.rs
+++ src/executor.rs
@@ -6,7 +6,6 @@ use register::Apsr;
 use core::Core;
 use memory::Fetch;
 
-use operation::sign_extend;
 use operation::add_with_carry;
 use operation::condition_passed;
 
@@ -22,10 +21,18 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
         None => panic!("undefined code"),
         Some(oper) => {
             match oper {
-                Op::MOV { rd, rm } => {
-                    println!("MOV");
-                    core.r[rd.value() as usize] = read_reg(core, rm);
-                    core.r[Reg::PC.value()] += 2;
+                Op::MOV_reg { rd, rm, setflags } => {
+                    println!("MOV {:?},{:?}", rd, rm);
+                    let result = read_reg(core, rm);
+                    core.r[rd.value() as usize] = result;
+
+                    if rd != Reg::PC {
+                        if setflags {
+                            core.apsr.set_n(result.get_bit(31));
+                            core.apsr.set_z(result == 0);
+                        }
+                        core.r[Reg::PC.value()] += 2;
+                    }
                 }
                 Op::BL { imm32 } => {
                     println!("BL");
@@ -38,19 +45,18 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
                     core.r[Reg::PC.value()] = read_reg(core, rm) & 0xfffffffe;
                 }
                 Op::BLX { rm } => {
-                    println!("BLX");
+                    println!("BLX R{}", rm.value());
                     let pc = read_reg(core, Reg::PC);
-                    core.r[Reg::LR.value()] = (pc - 2) | 0x01;
-                    core.r[Reg::PC.value()] = ((pc as i32) + (rm.value() as i32)) as u32;
+                    core.r[Reg::LR.value()] = (((pc - 2) >> 1) << 1) | 1;
+                    core.r[Reg::PC.value()] = read_reg(core, rm) & 0xfffffffe;
                 }
-                Op::MOV_imm8 { rd, imm8 } => {
-                    println!("MOV_imm8");
-                    core.r[rd.value()] = imm8 as u32;
+                Op::MOV_imm { rd, imm32 } => {
+                    println!("MOV_imm {:?}, {}", rd, imm32);
+                    core.r[rd.value()] = imm32 as u32;
                     core.r[Reg::PC.value()] += 2;
                 }
-                Op::B_imm8 { cond, imm8 } => {
-                    println!("B_imm8");
-                    let imm32 = sign_extend((imm8 as u32) << 1, 8, 32);
+                Op::B { cond, imm32 } => {
+                    println!("B{:?} {}", cond, imm32);
                     if condition_passed(cond, &core.apsr) {
                         let pc = read_reg(core, Reg::PC);
                         core.r[Reg::PC.value()] = ((pc as i32) + imm32) as u32;
@@ -58,16 +64,9 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
                         core.r[Reg::PC.value()] += 2;
                     }
                 }
-                Op::B_imm11 { imm11 } => {
-                    println!("B_imm11");
-                    let pc = read_reg(core, Reg::PC);
-                    let imm32 = sign_extend((imm11 as u32) << 1, 11, 32);
-                    core.r[Reg::PC.value()] = ((pc as i32) + imm32) as u32;
-                }
 
-                Op::CMP_imm8 { rn, imm8 } => {
-                    println!("CMP_imm8");
-                    let imm32 = imm8 as u32;
+                Op::CMP_imm { rn, imm32 } => {
+                    println!("CMP_imm {:?}, {}", rn, imm32);
                     let (result, carry, overflow) =
                         add_with_carry(read_reg(core, rn), imm32 ^ 0xFFFFFFFF, true);
                     core.apsr.set_n(result.get_bit(31));
@@ -77,7 +76,7 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
                     core.r[Reg::PC.value()] += 2;
                 }
                 Op::CMP { rn, rm } => {
-                    println!("CMP");
+                    println!("CMP {:?}, {:?}", rn, rm);
                     let (result, carry, overflow) =
                         add_with_carry(read_reg(core, rn), read_reg(core, rm) ^ 0xFFFFFFFF, true);
                     core.apsr.set_n(result.get_bit(31));
@@ -92,16 +91,19 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
                     let address = core.msp - 4 * (registers.len() as u32);
                     core.r[Reg::PC.value()] += 2;
                 }
-                Op::LDR { rt, imm8 } => {
-                    println!("LDR");
-                    let imm32 = (imm8 as u32) << 2;
-                    let pc = read_reg(core, Reg::PC);
-                    let address = (pc & 0xfffffffc) + imm32;
-                    core.r[rt.value()] = core.memory.fetch32(address);
+                Op::LDR_imm { rt, rn, imm32 } => {
+                    println!("LDR R{}, R{}, {}", rt.value(), rn.value(), imm32);
+                    let address = read_reg(core, rn) + imm32;
+                    core.r[rt.value()] = core.memory.fetch32(address & 0xfffffffc);
+                    core.r[Reg::PC.value()] += 2;
+                }
+                Op::LDR_lit { rt, imm32 } => {
+                    println!("LDR R{},[PC, #{}]", rt.value(), imm32);
+                    let base = read_reg(core, Reg::PC) & 0xfffffffc;
+                    core.r[rt.value()] = core.memory.fetch32(base + imm32);
                     core.r[Reg::PC.value()] += 2;
                 }
                 Op::ADD { rdn, rm } => {
-
                     println!("ADD");
                     let (result, carry, overflow) =
                         add_with_carry(read_reg(core, rdn), read_reg(core, rm), false);
@@ -109,10 +111,10 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
                     core.r[Reg::PC.value()] += 2;
                 }
                 Op::ADDS_imm { rn, rd, imm32 } => {
-                    println!("ADDS_imm R{:x}, R{:x}, #{:x}", rn.value(), rd.value(), imm32);
+                    println!("ADDS_imm R{:x}, R{:x}, #{}", rn.value(), rd.value(), imm32);
                     let r_n = read_reg(core, rn);
                     let (result, carry, overflow) =
-                        add_with_carry(read_reg(core, rn), imm32 as u32, false);
+                        add_with_carry(read_reg(core, rn), imm32, false);
 
                     core.apsr.set_n(result.get_bit(31));
                     core.apsr.set_z(result == 0);
@@ -124,7 +126,7 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
 
                 }
                 Op::ADDS { rm, rn, rd } => {
-                    println!("ADDS");
+                    println!("ADDS R{}, R{}, R{}", rn.value(), rd.value(), rm.value());
                     let (result, carry, overflow) =
                         add_with_carry(read_reg(core, rn), read_reg(core, rm), false);
                     core.apsr.set_n(result.get_bit(31));
@@ -137,7 +139,7 @@ pub fn execute<T: Fetch>(core: &mut Core<T>, op: Option<Op>) {
 
                 }
 
-                _ => panic!("unimplemented instruction") ,
+                _ => panic!("unimplemented instruction"),
             }
         }
     }
