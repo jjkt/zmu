2eebc6a9769a9965e526ce9485a7dfe7783ab2f5
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2017-12-12 18:25:10 +0200

    Refactor RAM and Flash implementations
    Instead of using cursor, have a boxed memory. The new api allows the user to define the required size.

--- zmu_cortex_m/src/memory/flash.rs
+++ zmu_cortex_m/src/memory/flash.rs
@@ -1,51 +1,40 @@
-use byteorder::{LittleEndian, ReadBytesExt};
-use std::io::{Cursor, Seek, SeekFrom};
+use byteorder::{ByteOrder, LittleEndian};
 use bus::Bus;
 
-
-pub struct FlashMemory<'a> {
+pub struct FlashMemory {
     start_address: u32,
-    access: Cursor<&'a mut [u8]>,
-    size: usize,
+    data: Box<[u8]>,
 }
 
-impl<'a> FlashMemory<'a> {
-    pub fn new(data: &'a mut [u8], start_address: u32) -> FlashMemory<'a> {
-        let len = data.len();
+impl FlashMemory {
+    pub fn new(start_address: u32, size: usize) -> FlashMemory {
+        let data = vec![0u8; size].into_boxed_slice();
 
         FlashMemory {
             start_address: start_address,
-            access: Cursor::new(data),
-            size: len,
+            data: data,
         }
+    }
+
+    pub fn load(&mut self, new_data: &[u8]) {
+        self.data.copy_from_slice(new_data);
     }
 }
 
-impl<'a> Bus for FlashMemory<'a> {
+impl Bus for FlashMemory {
     fn read8(&mut self, addr: u32) -> u8 {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        let value = self.access.read_u8().unwrap();
-        //print!("FLASH R8 [0x{:x}] => 0x{:x}\n", addr, value);
-        value
+        let a = addr - self.start_address;
+        self.data[a as usize]
     }
     fn read16(&mut self, addr: u32) -> u16 {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        let value = self.access.read_u16::<LittleEndian>().unwrap();
-        //print!("FLASH R16 [0x{:x}] => 0x{:x}\n", addr, value);
-        value
+        let a = (addr - self.start_address) as usize;
+
+        LittleEndian::read_u16(&self.data[a..a + 2])
     }
 
     fn read32(&mut self, addr: u32) -> u32 {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        let value = self.access.read_u32::<LittleEndian>().unwrap();
-        //print!("FLASH R32 [0x{:x}] => 0x{:x}\n", addr, value);
-        value
+        let a = (addr - self.start_address) as usize;
+        LittleEndian::read_u32(&self.data[a..a + 4])
     }
 
     fn write32(&mut self, addr: u32, value: u32) {
@@ -71,9 +60,45 @@ impl<'a> Bus for FlashMemory<'a> {
     }
 
     fn in_range(&self, addr: u32) -> bool {
-        if (addr >= self.start_address) && (addr <= (self.start_address + self.size as u32)) {
+        if (addr >= self.start_address) && (addr < (self.start_address + self.data.len() as u32)) {
             return true;
         }
         false
+    }
+}
+
+#[test]
+fn test_new() {
+    // should be able to make new instance of memory
+    let _mem = FlashMemory::new(0, 1024);
+}
+
+#[test]
+fn test_load() {
+    let mut mem = FlashMemory::new(0, 1024);
+    mem.load(&vec![42u8; 1024]);
+    assert_eq!(mem.read8(0), 42);
+    assert_eq!(mem.read16(0), (42 << 8) + 42);
+    assert_eq!(mem.read32(0), (42 << 24) + (42 << 16) + (42 << 8) + 42);
+}
+
+#[test]
+fn test_in_range() {
+    {
+        /* no offset */
+        let mem = FlashMemory::new(0, 1024);
+        assert!(mem.in_range(0));
+        assert!(mem.in_range(1023));
+        assert!(!mem.in_range(1024));
+        assert!(!mem.in_range(0xFFFF_FFFF));
+    }
+
+    {
+        /* offset of 0x8000_0000 */
+        let mem = FlashMemory::new(0x8000_0000, 1024);
+        assert!(mem.in_range(0x8000_0000));
+        assert!(mem.in_range(0x8000_0001));
+        assert!(!mem.in_range(0x8000_0000 + 1024));
+        assert!(!mem.in_range(0x8000_0000 + 0xffff));
     }
 }
--- zmu_cortex_m/src/memory/ram.rs
+++ zmu_cortex_m/src/memory/ram.rs
@@ -1,80 +1,117 @@
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
-use std::io::{Cursor, Seek, SeekFrom};
+use byteorder::{LittleEndian, ByteOrder};
 use bus::Bus;
 
 
-pub struct RAM<'a> {
+
+pub struct RAM {
     start_address: u32,
-    access: Cursor<&'a mut [u8]>,
-    size: usize,
+    data: Box<[u8]>,
 }
 
-impl<'a> RAM<'a> {
-    pub fn new(data: &'a mut [u8], start_address: u32) -> RAM<'a> {
-        let len = data.len();
+impl RAM {
+    pub fn new(start_address: u32, size: usize) -> RAM {
+        let data = vec![0u8; size].into_boxed_slice();
 
         RAM {
             start_address: start_address,
-            access: Cursor::new(data),
-            size: len,
+            data: data,
         }
     }
 }
 
-impl<'a> Bus for RAM<'a> {
-    fn read8(&mut self, addr: u32) -> u8 {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        let value = self.access.read_u8().unwrap();
-        //print!("RAM R8 [0x{:x}] => 0x{:x}\n", addr, value);
-        value
-    }
 
+impl Bus for RAM {
+    fn read8(&mut self, addr: u32) -> u8 {
+        let a = addr - self.start_address;
+        self.data[a as usize]
+    }
     fn read16(&mut self, addr: u32) -> u16 {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        let value = self.access.read_u16::<LittleEndian>().unwrap();
-        //print!("RAM R16 [0x{:x}] => 0x{:x}\n", addr, value);
-        value
+        let a = (addr - self.start_address) as usize;
+
+        LittleEndian::read_u16(&self.data[a..a + 2])
     }
 
     fn read32(&mut self, addr: u32) -> u32 {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        let value = self.access.read_u32::<LittleEndian>().unwrap();
-        //print!("RAM R32 [0x{:x}] => 0x{:x}\n", addr, value);
-        value
+        let a = (addr - self.start_address) as usize;
+        LittleEndian::read_u32(&self.data[a..a + 4])
+    }
+
+    fn write8(&mut self, addr: u32, value: u8) {
+        let a = addr - self.start_address;
+        self.data[a as usize] = value
+    }
+
+    fn write16(&mut self, addr: u32, value: u16) {
+        let a = (addr - self.start_address) as usize;
+
+        LittleEndian::write_u16(&mut self.data[a..a + 2], value)
     }
 
     fn write32(&mut self, addr: u32, value: u32) {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        self.access.write_u32::<LittleEndian>(value).unwrap();
-        //print!("RAM W32 [0x{:x}] <= 0x{:x}\n", addr, value);
-    }
-    fn write16(&mut self, addr: u32, value: u16) {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        self.access.write_u16::<LittleEndian>(value).unwrap();
-        //print!("RAM W32 [0x{:x}] <= 0x{:x}\n", addr, value);
-    }
-    fn write8(&mut self, addr: u32, value: u8) {
-        self.access
-            .seek(SeekFrom::Start(u64::from(addr - self.start_address)))
-            .unwrap();
-        self.access.write_u8(value).unwrap();
-        //print!("RAM W32 [0x{:x}] <= 0x{:x}\n", addr, value);
+        let a = (addr - self.start_address) as usize;
+        LittleEndian::write_u32(&mut self.data[a..a + 4], value)
     }
 
     fn in_range(&self, addr: u32) -> bool {
-        if (addr >= self.start_address) && (addr <= (self.start_address + self.size as u32)) {
+        if (addr >= self.start_address) && (addr < (self.start_address + self.data.len() as u32)) {
             return true;
         }
         false
+    }
+}
+
+
+#[test]
+fn test_new() {
+    // should be able to make new instance of memory
+    let _mem = RAM::new(0, 1024);
+}
+
+
+#[test]
+fn test_in_range() {
+    {
+        /* no offset */
+        let mem = RAM::new(0, 1024);
+        assert!(mem.in_range(0));
+        assert!(mem.in_range(1023));
+        assert!(!mem.in_range(1024));
+        assert!(!mem.in_range(0xFFFF_FFFF));
+    }
+
+    {
+        /* offset of 0x8000_0000 */
+        let mem = RAM::new(0x8000_0000, 1024);
+        assert!(mem.in_range(0x8000_0000));
+        assert!(mem.in_range(0x8000_0001));
+        assert!(!mem.in_range(0x8000_0000 + 1024));
+        assert!(!mem.in_range(0x8000_0000 + 0xffff));
+    }
+}
+
+#[test]
+fn test_write_read() {
+    {
+        let mut mem = RAM::new(0, 1024);
+        mem.write32(0, 0xAABBCCDD);
+        assert_eq!(mem.read32(0), 0xAABBCCDD);
+        mem.write32(1020, 0xAABBCCDD);
+        assert_eq!(mem.read32(1020), 0xAABBCCDD);
+    }
+
+    {
+        let mut mem = RAM::new(0, 1024);
+        mem.write16(0, 0xAABB);
+        assert_eq!(mem.read16(0), 0xAABB);
+        mem.write16(1022, 0xCCDD);
+        assert_eq!(mem.read16(1022), 0xCCDD);
+    }
+
+    {
+        let mut mem = RAM::new(0, 1024);
+        mem.write8(0, 0xAA);
+        assert_eq!(mem.read8(0), 0xAA);
+        mem.write8(1022, 0xCC);
+        assert_eq!(mem.read8(1022), 0xCC);
     }
 }
