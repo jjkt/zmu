3b36e6b9ad608668e9545c1a18280d1fdd78f176
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2018-01-01 08:15:37 +0200

    Add more bit decoding funcs
    Inlined bit decodings are usefuly replacement for get_bits.

--- zmu_cortex_m/src/core/bits.rs
+++ zmu_cortex_m/src/core/bits.rs
@@ -4,6 +4,11 @@ pub fn bits_0_3(n: u16) -> u8 {
 }
 
 #[inline]
+pub fn bits_0_4(n: u16) -> u8 {
+    n as u8 & 0b_1111
+}
+
+#[inline]
 pub fn bits_0_7(n: u16) -> u8 {
     (n & 0b_111_1111) as u8
 }
@@ -11,6 +16,51 @@ pub fn bits_0_7(n: u16) -> u8 {
 #[inline]
 pub fn bits_0_8(n: u16) -> u8 {
     (n & 0b_1111_1111) as u8
+}
+
+#[inline]
+pub fn bit_0(n: u16) -> u8 {
+    ((n & 0b_0000_0001) >> 0) as u8
+}
+
+#[inline]
+pub fn bit_1(n: u16) -> u8 {
+    ((n & 0b_0000_0010) >> 1) as u8
+}
+
+#[inline]
+pub fn bit_2(n: u16) -> u8 {
+    ((n & 0b_0000_0100) >> 2) as u8
+}
+
+#[inline]
+pub fn bit_3(n: u16) -> u8 {
+    ((n & 0b_0000_1000) >> 3) as u8
+}
+
+#[inline]
+pub fn bit_4(n: u16) -> u8 {
+    ((n & 0b_0001_0000) >> 4) as u8
+}
+
+#[inline]
+pub fn bit_5(n: u16) -> u8 {
+    ((n & 0b_0010_0000) >> 5) as u8
+}
+
+#[inline]
+pub fn bit_6(n: u16) -> u8 {
+    ((n & 0b_0100_0000) >> 6) as u8
+}
+
+#[inline]
+pub fn bit_7(n: u16) -> u8 {
+    ((n & 0b_1000_0000) >> 7) as u8
+}
+
+#[inline]
+pub fn bit_8(n: u16) -> u8 {
+    ((n & 0b_1_0000_0000) >> 8) as u8
 }
 
 #[inline]
@@ -42,8 +92,11 @@ pub fn bits_8_11(n: u16) -> u8 {
 }
 
 #[inline]
+pub fn bits_8_12(n: u16) -> u8 {
+    ((n & 0b_1111_00_000_000) >> 8) as u8
+}
+
+#[inline]
 pub fn bits_6_11(n: u16) -> u8 {
     ((n & 0b_11111_000_000) >> 6) as u8
 }
-
-
