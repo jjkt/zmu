e89acb036dc88632ed01f902e19371694a7c922f
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2018-12-15 12:43:26 +0200

    MOVT: return UDF for now
    unimplemented!() would not show which opcode was unimplemented when the panic happened.

--- zmu_cortex_m/src/decoder/movt.rs
+++ zmu_cortex_m/src/decoder/movt.rs
@@ -1,6 +1,11 @@
 use crate::core::instruction::Instruction;
+use crate::core::ThumbCode;
+
 
 #[allow(non_snake_case)]
-pub fn decode_MOVT_t1(_opcode: u32) -> Instruction {
-    unimplemented!()
+pub fn decode_MOVT_t1(opcode: u32) -> Instruction {
+    Instruction::UDF {
+        imm32: 0,
+        opcode: ThumbCode::from(opcode),
+    }
 }
