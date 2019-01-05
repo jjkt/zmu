f8165dd02595426adef9410718cc2d21494bd041
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2019-01-05 10:53:58 +0200

    Add test for rust hello world
    Run the cortex-m3 version from rust book's hello world example

--- testall.sh
+++ testall.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 
+#
+# hello world
+#
 cd tests/hello_world
 make clean
 make
@@ -10,3 +13,26 @@ cd ../..
 ./target/release/zmu run tests/hello_world/hello_world-cm3.elf
 ./target/release/zmu run tests/hello_world/hello_world-cm4.elf
 ./target/release/zmu run tests/hello_world/hello_world-cm4f.elf
+
+
+#
+# rust hello world
+cd tests/hello_world_rust
+cargo build --example hello
+cd ../..
+./target/release/zmu run tests/hello_world_rust/target/thumbv7m-none-eabi/debug/examples/hello
+
+
+#
+# pi
+#
+cd tests/pi
+make clean
+make
+cd ../..
+./target/release/zmu run tests/pi/pi-cm0.elf
+./target/release/zmu run tests/pi/pi-cm0p.elf
+./target/release/zmu run tests/pi/pi-cm3.elf
+./target/release/zmu run tests/pi/pi-cm4.elf
+#./target/release/zmu run tests/pi/pi-cm4f.elf
+
