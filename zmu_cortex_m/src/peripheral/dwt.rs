eb3171e2cac36d9c740d2825db22b291d72a7edd
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2019-03-12 13:24:17 +0200

    DWT: Supports cycle counter

--- zmu_cortex_m/src/peripheral/dwt.rs
+++ zmu_cortex_m/src/peripheral/dwt.rs
@@ -2,6 +2,7 @@
 //! Cortex Debug and Trace unit simulation
 //!
 
+use crate::core::bits::Bits;
 use crate::Processor;
 
 /// Register API to Debug and Trace peripheral
@@ -15,14 +16,26 @@ pub trait Dwt {
     /// write cycle counter value
     ///
     fn write_cyccnt(&mut self, value: u32);
+
+    ///
+    /// clock dwt block
+    ///
+    fn dwt_tick(&mut self);
 }
 
 impl Dwt for Processor {
     fn write_ctrl(&mut self, value: u32) {
-        self.dwt_ctrl = value;
+        self.dwt_ctrl.set_bits(16..23, value.get_bits(16..23));
+        self.dwt_ctrl.set_bits(0..13, value.get_bits(0..13));
     }
 
     fn write_cyccnt(&mut self, value: u32) {
         self.dwt_cyccnt = value;
+    }
+
+    fn dwt_tick(&mut self) {
+        if self.dwt_ctrl.get_bit(0) {
+            self.dwt_cyccnt += 1;
+        }
     }
 }
