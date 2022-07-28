5e15e81bc5e4a35ba8046912da0a493beaa69935
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2022-07-28 19:06:59 +0300

    Update cortex-m-rtic tests

--- test_cortex-m-rtic.sh
+++ test_cortex-m-rtic.sh
@@ -17,13 +17,13 @@ else
 fi
 
 
-declare -a arr=("baseline" "binds" "capacity" "cfg"  "destructure" "generics" "hardware" "idle" "init" "late" "lock" "message" "not-send" "not-sync" "only-shared-access" "periodic" "pool" "preempt" "resource"  "schedule" "smallest" "shared-with-init" "task" "types")
+declare -a arr=("big-struct-opt" "binds" "capacity" "cancel-reschedule" "cfg-whole-task"  "common" "complex" "declared_locals" "destructure" "extern_binds" "extern_spawn" "generics" "hardware" "idle-wfi" "idle" "init" "locals" "lock-free" "lock" "message_passing" "message" "multilock" "not-sync" "only-shared-access" "periodic-at" "periodic-at2" "periodic" "pool" "preempt" "resource-user-struct" "schedule" "shared" "smallest" "spawn" "static" "t-binds" "t-cfg-resources" "t-htask-main" "t-idle-main" "t-late-not-send" "t-schedule" "t-spawn" "task" )
 cd tests/cortex-m-rtic
 cargo build --target thumbv7m-none-eabi
 for i in "${arr[@]}"
 do
-   echo "cargo build --target thumbv7m-none-eabi --features="__v7" --example $i"
-   cargo build --target thumbv7m-none-eabi --features="__v7" --example $i
+   echo "cargo build --target thumbv7m-none-eabi --example $i"
+   cargo build --target thumbv7m-none-eabi --example $i
 done
 cd ../..
 
