d07a12bf110b276d75c121d523f41dace3752d6d
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2025-11-30 12:48:48 +0200

    IT flags condition tracing

--- src/trace.rs
+++ src/trace.rs
@@ -2,11 +2,12 @@ extern crate zmu_cortex_m;
 
 use pad::PadStr;
 use std::collections::HashMap;
+use zmu_cortex_m::Processor;
+use zmu_cortex_m::core::condition::decode_itstate;
 use zmu_cortex_m::core::fetch::Fetch;
 use zmu_cortex_m::core::register::{Apsr, PSR};
 use zmu_cortex_m::core::thumb::ThumbCode;
 use zmu_cortex_m::decoder::Decoder;
-use zmu_cortex_m::Processor;
 
 pub fn format_trace_entry(processor: &Processor, symboltable: &HashMap<u32, &str>) -> String {
     let pc = processor.last_pc;
@@ -28,20 +29,40 @@ pub fn format_trace_entry(processor: &Processor, symboltable: &HashMap<u32, &str
         value: processor.psr.value,
     };
 
+    let itstate = processor.get_itstate();
+    let it_info = match decode_itstate(itstate) {
+        Some(decoded) => format!("IT:{:02x}({})", itstate, decoded),
+        None => format!("IT:{:02x}      ", itstate),
+    };
+
     format!(
-        "{0:}  {1:} {2:08X}  {3:}  {4:} {5:}{6:}{7:}{8:}{9:} r0:{10:08x} 1:{11:08x} 2:{12:08x} 3:{13:08x} 4:{14:08x} 5:{15:08x} 6:{16:08x} 7:{17:08x} 8:{18:08x} 9:{19:08x} 10:{20:08x} 11:{21:08x} 12:{22:08x} msp:{23:08x} psp:{24:08x} lr:{25:08x}",
-        opcode_str, instruction_str, pc, symbol, processor.instruction_count,
-        if psr.get_q() {'Q'} else {'q'},
-        if psr.get_v() {'V'} else {'v'},
-        if psr.get_c() {'C'} else {'c'},
-        if psr.get_z() {'Z'} else {'z'},
-        if psr.get_n() {'N'} else {'n'},
+        "{0:}  {1:} {2:08X}  {3:}  {4:} {5:}{6:}{7:}{8:}{9:} r0:{10:08x} 1:{11:08x} 2:{12:08x} 3:{13:08x} 4:{14:08x} 5:{15:08x} 6:{16:08x} 7:{17:08x} 8:{18:08x} 9:{19:08x} 10:{20:08x} 11:{21:08x} 12:{22:08x} msp:{23:08x} psp:{24:08x} lr:{25:08x} {26:}",
+        opcode_str,
+        instruction_str,
+        pc,
+        symbol,
+        processor.instruction_count,
+        if psr.get_q() { 'Q' } else { 'q' },
+        if psr.get_v() { 'V' } else { 'v' },
+        if psr.get_c() { 'C' } else { 'c' },
+        if psr.get_z() { 'Z' } else { 'z' },
+        if psr.get_n() { 'N' } else { 'n' },
         processor.r0_12[0],
-        processor.r0_12[1], processor.r0_12[2], processor.r0_12[3], processor.r0_12[4], processor.r0_12[5],
-        processor.r0_12[6], processor.r0_12[7], processor.r0_12[8], processor.r0_12[9], processor.r0_12[10],
+        processor.r0_12[1],
+        processor.r0_12[2],
+        processor.r0_12[3],
+        processor.r0_12[4],
+        processor.r0_12[5],
+        processor.r0_12[6],
+        processor.r0_12[7],
+        processor.r0_12[8],
+        processor.r0_12[9],
+        processor.r0_12[10],
         processor.r0_12[11],
-        processor.r0_12[12],processor.msp,
+        processor.r0_12[12],
+        processor.msp,
         processor.psp,
-        processor.lr
+        processor.lr,
+        it_info
     )
 }
